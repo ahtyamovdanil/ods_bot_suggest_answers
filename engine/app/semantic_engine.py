7d1779ce3a3eadd69578bc806ef3cda4c7abcacd
ahtyamovdanil <ahtyamovdanil@gmail.com>
2021-02-20 21:08:29 +0300

    добавлена документация для SemanticEngine

--- engine/app/semantic_engine.py
+++ engine/app/semantic_engine.py
@@ -5,7 +5,7 @@ from typing import List, Dict
 
 
 class SemanticEngine:
-    def __init__(self, text_df: pd.DataFrame, model=None) -> None:
+    def __init__(self, text_df: pd.DataFrame) -> None:
         """
         Args:
             text_df (pd.DataFrame): pandas dataframe with fields: ts, text
@@ -15,21 +15,24 @@ class SemanticEngine:
         self.embeddings = None
 
     def load_embeddings(self, path) -> None:
+        """ load embeddings from pickle file """
         with open(path, "rb") as file:
             self.embeddings = pickle.load(file)
 
     def save_embeddings(self, path) -> None:
+        """ save embeddings to pickle file """
         with open(path, "wb") as file:
             pickle.dump(self.embeddings, file)
 
     def calc_embeddings(self, corpus: List[str]):
+        """ calculate new embeddings """
         corpus_embeddings = self.model.encode(
             corpus, convert_to_tensor=True, show_progress_bar=False
         )
         self.embeddings = corpus_embeddings
 
     def get_top_k(self, query: str, k=5) -> List[Dict]:
-        """Get k most similar to query sentences
+        r"""Get k most similar to query sentences
         You need to call load_embeddings or calc_embeddings first to use this method
         Args:
             query (str): text for which you want to find similar sentences
@@ -42,10 +45,28 @@ class SemanticEngine:
                 score: cosin similarity score
                 text: message text
             }
+        Example 1: calculate embeddings, save them and get top 5 sentences :: 
+            >>> df = pd.read_csv("./data/prepared/edu_courses.tsv", sep="\t")
+            >>> engine = SemanticEngine(text_df=df)
+            >>> engine.calc_embeddings(df.text.tolist())
+            >>> engine.save_embeddings("./data/embeddings/edu_courses.pkl")
+            >>> query = "посоветуйте каких-нибудь курсов по pytorch"
+            >>> result = engine.get_top_k(query, k=5)
+            >>> for res in result:
+            ...     print(res["ts"], res["text"], res["score"], sep="\n")
+
+        Example 2: load embeddings from file, and get top 5 sentences
+            >>> df = pd.read_csv("./data/prepared/edu_courses.tsv", sep="\t")
+            >>> engine = SemanticEngine(text_df=df)
+            >>> engine.load_embeddings("./data/embeddings/edu_courses.pkl")
+            >>> query = "посоветуйте каких-нибудь курсов по pytorch"
+            >>> result = engine.get_top_k(query, k=5)
+            >>> for res in result:
+            ...     print(res["ts"], res["text"], res["score"], sep="\n")
         """
         if self.embeddings is None:
             raise ValueError(
-                "embeddings are not initialized. Call load_embeddings or calc_embeddings first"
+                "embeddings are not initialized. Call `load_embeddings` or `calc_embeddings` first"
             )
         query_embedding = self.model.encode(
             [query], convert_to_tensor=True, show_progress_bar=False
@@ -62,19 +83,4 @@ class SemanticEngine:
             if hit["score"] != 1
         ]
         return result
-
-
-if __name__ == "__main__":
-    """Usage example"""
-    df = pd.read_csv("./data/prepared/edu_courses.tsv", sep="\t")
-    df.dropna(inplace=True)
-    engine = SemanticEngine(text_df=df)
-    engine.calc_embeddings(df.text.tolist())
-    engine.save_embeddings("./data/embeddings/edu_courses.pkl")
-    engine.load_embeddings("./data/embeddings/edu_courses.pkl")
-    query = "посоветуйте каких-нибудь курсов по pytorch"
-    result = engine.get_top_k(query, k=5)
-
-    for res in result:
-        print(res["ts"], res["text"], res["score"], sep="\n")
-        print("---------------")
+        
